d308894b4f87c24414bc5a60031ccef5fea68064
FilipKoniuszewski <82803009+FilipKoniuszewski@users.noreply.github.com>
2021-08-24 01:21:20 +0200

    1 task done

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 function switchTheme() {
-  // TODO: implement toggling the light/dark theme
+    const dark_mode = document.getElementById("themeSwitch");
+    const body = document.body;
+    const nav_bar = document.getElementsByClassName("navbar navbar-expand-lg navbar-light bg-light");
+    if (dark_mode.innerHTML === "Activate Dark Theme") {
+        body.classList.add("bg-dark");
+        nav_bar.className += " navbar-dark";
+        // nav_bar.className += " bg-dark";
+        dark_mode.innerHTML = "Disable Dark Theme";
+    }
+    else {
+        body.removeAttribute("class");
+        // nav_bar.classList.remove("bg-dark", "navbar-dark");
+        dark_mode.innerHTML = "Activate Dark Theme";
+    }
 }
