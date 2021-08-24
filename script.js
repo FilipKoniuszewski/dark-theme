d9ab9beff3863608c4998d2769769055e20b8887
FilipKoniuszewski <82803009+FilipKoniuszewski@users.noreply.github.com>
2021-08-24 14:17:44 +0200

    3 tasks done

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 function switchTheme() {
     const dark_mode = document.getElementById("themeSwitch");
     const body = document.body;
-    const nav_bar = document.getElementsByClassName("navbar navbar-expand-lg navbar-light bg-light");
-    if (dark_mode.innerHTML === "Activate Dark Theme") {
+    const nav_bar = document.getElementsByClassName("navbar navbar-expand-lg navbar-light bg-light")[0];
+    const cards = document.getElementsByClassName("card");
+    if (dark_mode.innerText === "Activate Dark Theme") {
         body.classList.add("bg-dark");
-        nav_bar.className += " navbar-dark";
-        // nav_bar.className += " bg-dark";
+        nav_bar.className += " navbar-dark" + " bg-dark";
         dark_mode.innerHTML = "Disable Dark Theme";
+        for (let i = 0;i < cards.length; i++) {
+            cards[i].className += " text-white" + " bg-dark" + " border-light";
+        }
     }
     else {
         body.removeAttribute("class");
-        // nav_bar.classList.remove("bg-dark", "navbar-dark");
+        nav_bar.classList.remove("bg-dark", "navbar-dark");
+        for (let i = 0;i < cards.length; i++) {
+            cards[i].classList.remove("text-white", "bg-dark", "border-light");
+        }
         dark_mode.innerHTML = "Activate Dark Theme";
     }
 }
